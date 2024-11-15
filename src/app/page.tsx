dfdc241602cac955a1de43367dd3cba1ea9e273f
Dlurak <84224239+Dlurak@users.noreply.github.com>
2024-11-15 15:11:03 +0100

    Add a flake.nix and a new toast

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,61 +5,64 @@ import { FinishedView, QuestionView } from "@/components/Views";
 import { toast } from "react-toastify";
 import { Country } from "@/types/Country";
 import { Locale } from "@/constants/countries";
-import useLocalStorage from "use-local-storage"
+import useLocalStorage from "use-local-storage";
 import { Cog6ToothIcon } from "@heroicons/react/24/outline";
 
 type Count = Record<string, number>;
 
 export default function Home() {
-	const [countryCounts, setCountryCounts] = useState<Count>({});
+  const [countryCounts, setCountryCounts] = useState<Count>({});
 
-	const incCounter = (entry: Country) => {
-		setCountryCounts((old) => ({
-			...old,
-			[entry[0]]: (old[entry[0]] ?? 0) + 1,
-		}));
-	};
+  const incCounter = (entry: Country) => {
+    setCountryCounts((old) => ({
+      ...old,
+      [entry[0]]: (old[entry[0]] ?? 0) + 1,
+    }));
+  };
 
-	const [locale] = useLocalStorage<Locale>("locale", "de");
-	const [usedLocale, setUsedLocale] = useState<Locale>("de")
-	const [validLocale, setValidLocale] = useState(false)
+  const [locale] = useLocalStorage<Locale>("locale", "de");
+  const [usedLocale, setUsedLocale] = useState<Locale>("de");
+  const [validLocale, setValidLocale] = useState(false);
 
-	useEffect(() => {
-	setUsedLocale(locale)
-		setValidLocale(true)
-		}, [])
+  useEffect(() => {
+    setUsedLocale(locale);
+    setValidLocale(true);
+  }, []);
 
-	const { countries, correct, incorrect, skip } = useCountries({
-		lang: usedLocale,
-		onCorrect: incCounter,
-		onIncorrect: incCounter,
-	});
+  const { countries, correct, incorrect, skip } = useCountries({
+    lang: usedLocale,
+    onCorrect: incCounter,
+    onIncorrect: incCounter,
+  });
 
-	return (
-		<>
-			<div className="h-[100dvh] relative">
-				<a className="w-8 h-8 fixed top-4 right-4" href="/settings">
-					<Cog6ToothIcon />
-				</a>
-				{countries.length >= 1 && validLocale && (
-					<QuestionView
-						country={countries[0]}
-						onCorrect={() => {
-							toast.success("That was correct");
-							correct();
-						}}
-						onIncorrect={(entry) => {
-							toast.error(`That was ${entry[0]}`);
-							incorrect();
-						}}
-						onSkip={skip}
-						lang={usedLocale}
-					/>
-				)}
-				{countries.length === 0 && (
-					<FinishedView counts={countryCounts} lang={usedLocale} />
-				)}
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div className="h-[100dvh] relative">
+        <a className="w-8 h-8 fixed top-4 right-4" href="/settings">
+          <Cog6ToothIcon />
+        </a>
+        {countries.length >= 1 && validLocale && (
+          <QuestionView
+            country={countries[0]}
+            onCorrect={() => {
+              toast.success("That was correct");
+              correct();
+            }}
+            onIncorrect={([country]) => {
+              toast.error(`That was ${country}`);
+              incorrect();
+            }}
+            onSkip={([country]) => {
+              toast.info(`That was ${country}`);
+              skip();
+            }}
+            lang={usedLocale}
+          />
+        )}
+        {countries.length === 0 && (
+          <FinishedView counts={countryCounts} lang={usedLocale} />
+        )}
+      </div>
+    </>
+  );
 }
--- src/hooks/countries.ts
+++ src/hooks/countries.ts
@@ -4,47 +4,49 @@ import { Country } from "@/types/Country";
 import { Locale, langs } from "@/constants/countries";
 
 interface UseCountryProps {
-	lang: Locale
+  lang: Locale;
 
-	limit?: number;
+  limit?: number;
 
-	onCorrect?: (country: Country) => void;
-	onIncorrect?: (country: Country) => void;
-	onSkip?: (country: Country) => void;
+  onCorrect?: (country: Country) => void;
+  onIncorrect?: (country: Country) => void;
+  onSkip?: (country: Country) => void;
 }
 
 export const useCountries = (props: UseCountryProps) => {
-	const [countries, setCountries] = useState(
-		Object.entries(langs[props.lang]).slice(0, props.limit ?? Infinity),
-	);
+  const [countries, setCountries] = useState(
+    Object.entries(langs[props.lang]).slice(0, props.limit ?? Infinity),
+  );
 
-	useEffect(() => {
-		setCountries(shuffle(countries));
-	}, []);
+  useEffect(() => {
+    setCountries(shuffle(countries));
+  }, []);
 
-	useEffect(() => {
-		setCountries(shuffle(
-		Object.entries(langs[props.lang]).slice(0, props.limit ?? Infinity),
-		))
-	}, [props.lang, props.limit])
+  useEffect(() => {
+    setCountries(
+      shuffle(
+        Object.entries(langs[props.lang]).slice(0, props.limit ?? Infinity),
+      ),
+    );
+  }, [props.lang, props.limit]);
 
-	return {
-		correct() {
-			if (props.onCorrect) props.onCorrect(countries[0]);
-			setCountries(countries.slice(1));
-		},
-		incorrect() {
-			const first = countries[0];
-			if (props.onIncorrect) props.onIncorrect(first);
+  return {
+    correct() {
+      if (props.onCorrect) props.onCorrect(countries[0]);
+      setCountries(countries.slice(1));
+    },
+    incorrect() {
+      const first = countries[0];
+      if (props.onIncorrect) props.onIncorrect(first);
 
-			setCountries([...countries.slice(1), first]);
-		},
-		skip() {
-			const first = countries[0];
-			if (props.onSkip) props.onSkip(first);
+      setCountries([...countries.slice(1), first]);
+    },
+    skip() {
+      const first = countries[0];
+      if (props.onSkip) props.onSkip(first);
 
-			setCountries([...countries.slice(1), first]);
-		},
-		countries,
-	};
+      setCountries([...countries.slice(1), first]);
+    },
+    countries,
+  };
 };
